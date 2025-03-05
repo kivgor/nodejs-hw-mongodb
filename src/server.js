40ca16840dd11ed611b0a5faeea61cbae04bc16a
Igor <pa220282kiv@gmail.com>
2025-03-05 14:25:54 +0200

    Add all steps

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -18,6 +19,40 @@ export const setupServer = () => {
       },
     }),
   );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message:
+        'Use http://localhost:3000/contacts to get all contacts or add /contactId to get contact by Id',
+    });
+  });
+
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contact,
+    });
+  });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
