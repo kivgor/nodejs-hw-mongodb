ea19f2ae223626df2e0e521f7c792b5d06dac3df
Igor <pa220282kiv@gmail.com>
2025-04-01 15:28:14 +0300

    Add field userId for post /contacts

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -45,6 +45,8 @@ export const getContactByIdController = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res) => {
+  req.body.userId = req.user._id;
+
   const contact = await createContact(req.body);
 
   res.status(201).json({
